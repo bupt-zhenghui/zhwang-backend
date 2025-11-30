3a3c8a94d1e310092d84257a04bd72b118d8c26f
bupt-zhenghui <wangzhenghui@bupt.edu.cn>
2025-11-30 14:49:29 +0800

    - add reload-nginx.sh

---
+++ deployment/reload-nginx.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# nginx配置调整 & 重启
+NGINX_CONF_ABS_PATH=$(realpath ./nginx.conf)
+
+if [ ! -f "$NGINX_CONF_ABS_PATH" ]; then
+  echo "错误：当前目录下未找到 nginx.conf 文件"
+  exit 1
+fi
+
+# 复制文件到目标路径，-f 强制覆盖已存在的文件
+sudo cp -f "$NGINX_CONF_ABS_PATH" /etc/nginx/nginx.conf
+
+# 检查复制是否成功
+if [ $? -eq 0 ]; then
+  echo "成功将 $NGINX_CONF_ABS_PATH 复制到 /etc/nginx/nginx.conf（已覆盖现有文件）"
+  nginx -s reload
+else
+  echo "复制失败，请检查权限或文件路径"
+  exit 1
+fi
