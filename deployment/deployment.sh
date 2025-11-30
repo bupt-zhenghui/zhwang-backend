15ca1f8774873111b2c327b899045831f087c2d0
bupt-zhenghui <wangzhenghui@bupt.edu.cn>
2025-11-30 15:21:20 +0800

    - revise deployment shell

--- deployment/deployment.sh
+++ deployment/deployment.sh
@@ -1,32 +1,15 @@
 #!/bin/bash
 
-MKDOCS_TAG="v2-x86"
-MKDOCS_PORT="9090"
+PROJ_NAME="mkdocs"
+PROJ_TAG="v2-x86"
+PROJ_PORT="9090"
 DOCKER_PWD="Wangzhenghui123"
 
 docker login --username=bupt_wzh --password="${DOCKER_PWD}" crpi-jic5w57mqp9e5onw.cn-hangzhou.personal.cr.aliyuncs.com
 
-docker pull "crpi-jic5w57mqp9e5onw.cn-hangzhou.personal.cr.aliyuncs.com/zhwang/mkdocs:${MKDOCS_TAG}"
+docker pull "crpi-jic5w57mqp9e5onw.cn-hangzhou.personal.cr.aliyuncs.com/zhwang/${PROJ_NAME}:${PROJ_TAG}"
 
-docker run -p "${MKDOCS_PORT}:8000" --name mkdocs -d "crpi-jic5w57mqp9e5onw.cn-hangzhou.personal.cr.aliyuncs.com/zhwang/mkdocs:${MKDOCS_TAG}"
+docker rm -f "${PROJ_NAME}"
 
+docker run -p "${PROJ_PORT}:8000" --name "${PROJ_NAME}" -d "crpi-jic5w57mqp9e5onw.cn-hangzhou.personal.cr.aliyuncs.com/zhwang/${PROJ_NAME}:${PROJ_TAG}"
 
-# nginx配置调整 & 重启
-NGINX_CONF_ABS_PATH=$(realpath ./nginx.conf)
-
-if [ ! -f "$NGINX_CONF_ABS_PATH" ]; then
-  echo "错误：当前目录下未找到 nginx.conf 文件"
-  exit 1
-fi
-
-# 复制文件到目标路径，-f 强制覆盖已存在的文件
-sudo cp -f "$NGINX_CONF_ABS_PATH" /etc/nginx/nginx.conf
-
-# 检查复制是否成功
-if [ $? -eq 0 ]; then
-  echo "成功将 $NGINX_CONF_ABS_PATH 复制到 /etc/nginx/nginx.conf（已覆盖现有文件）"
-  nginx -s reload
-else
-  echo "复制失败，请检查权限或文件路径"
-  exit 1
-fi
--- mainpage/deployment.sh
+++ mainpage/deployment.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-MAINPAGE_TAG="v2-x86"
-MAINPAGE_PORT="7999"
+PROJ_NAME = "mainpage"
+PROJ_TAG="v2-x86"
+PROJ_PORT="7999"
 DOCKER_PWD="Wangzhenghui123"
 
 docker login --username=bupt_wzh --password="${DOCKER_PWD}" crpi-jic5w57mqp9e5onw.cn-hangzhou.personal.cr.aliyuncs.com
 
-docker pull "crpi-jic5w57mqp9e5onw.cn-hangzhou.personal.cr.aliyuncs.com/zhwang/mainpage:${MAINPAGE_TAG}"
+docker pull "crpi-jic5w57mqp9e5onw.cn-hangzhou.personal.cr.aliyuncs.com/zhwang/${PROJ_NAME}:${PROJ_TAG}"
 
-docker run -p "${MKDOCS_PORT}:80" --name mainpage -d "crpi-jic5w57mqp9e5onw.cn-hangzhou.personal.cr.aliyuncs.com/zhwang/mainpage:${MAINPAGE_TAG}"
+docker rm -f "${PROJ_NAME}"
+
+docker run -p "${PROJ_PORT}:80" --name "${PROJ_NAME}" -d "crpi-jic5w57mqp9e5onw.cn-hangzhou.personal.cr.aliyuncs.com/zhwang/${PROJ_NAME}:${PROJ_TAG}"
